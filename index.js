2a0bc1c173ffe9747285e7eb2b80f3d1cda85c50
scarletstormcat <60307510+scarletstormcat@users.noreply.github.com>
2023-06-20 12:39:43 -0700

    testing feature

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function login() {
     player.name = inputValue
     
     gameContEl.hidden = false
+    newCardBtnEl.hidden = true
     loginEl.hidden = true
     playerEl.textContent = player.name + ": $" + player.chips
    
